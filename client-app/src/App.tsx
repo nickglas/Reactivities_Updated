89a0423fc09a712049f04b3e73694c9e52acbfa0
nickglas <40456410+nickglas@users.noreply.github.com>
2021-04-17 01:44:56 +0200

    End of section 3

---
+++ client-app/src/App.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios'
+import { Header, List } from 'semantic-ui-react';
+
+const App = () => {
+
+  const [activities, setActivities] = useState([])
+
+  useEffect(() => {
+    axios.get('https://localhost:5001/api/activities').then(response => setActivities(response.data));
+  }, [])
+
+  return (
+    <div>
+      <Header as='h2' icon='users' content='Reactivities' className="App-header" />
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
+}
+
+export default App;
