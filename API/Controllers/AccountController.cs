a26ec240330d1ca48f6a8fae6ad3e76f1df95c7f
Nick Glas <nickglas@hotmail.nl>
2021-08-10 13:37:28 +0200

    FB login complete

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using API.DTOs;
@@ -8,6 +11,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace API.Controllers
 {
@@ -19,12 +24,23 @@ namespace API.Controllers
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
         private readonly TokenService _tokenService;
-        public AccountController(UserManager<AppUser> userManager,
-        SignInManager<AppUser> signInManager, TokenService tokenService)
+        private readonly IConfiguration _config;
+        private readonly HttpClient _httpClient;
+        
+        public AccountController(
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager, 
+            TokenService tokenService,
+            IConfiguration config)
         {
             _tokenService = tokenService;
+            _config = config;
             _signInManager = signInManager;
             _userManager = userManager;
+            _httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri("https://graph.facebook.com")
+            };
         }
 
         [HttpPost("login")]
@@ -86,6 +102,45 @@ namespace API.Controllers
             return CreateUserObject(user);
         }
 
+        [HttpPost("fbLogin")]
+        public async Task<ActionResult<UserDto>> FacebookLogin(string accessToken)
+        {
+            var fbVerifyKeys = _config["Facebook:AppId"] + "|" + _config["Facebook:AppSecret"];
+
+            var verifyToken = await _httpClient.GetAsync($"debug_token?input_token={accessToken}&access_token={fbVerifyKeys}");
+
+            if (!verifyToken.IsSuccessStatusCode) return Unauthorized();
+
+            var fbUrl = $"me?access_token={accessToken}&fields=name,email,picture.width(100).height(100)";
+
+            var response = await _httpClient.GetAsync(fbUrl);
+
+            if (!response.IsSuccessStatusCode) return Unauthorized();
+
+            var fbInfo = JsonConvert.DeserializeObject<dynamic>(await response.Content.ReadAsStringAsync());
+
+            var username = (string)fbInfo.id;
+
+            var user = await _userManager.Users.Include(x=>x.Photos).FirstOrDefaultAsync(x=>x.UserName == username);
+
+            if (user != null) return CreateUserObject(user);
+
+            user = new AppUser()
+            {
+                DisplayName = (string) fbInfo.name,
+                Email = (string) fbInfo.email,
+                UserName = (string) fbInfo.id,
+                Photos = new List<Photo>
+                    {new Photo {Id = "fb_" + (string) fbInfo.id, Url = (string) fbInfo.picture.data.url, IsMain = true}}
+            };
+
+            var result = await _userManager.CreateAsync(user);
+
+            if (!result.Succeeded) return BadRequest("Problem creating user account");
+
+            return CreateUserObject(user);
+        }
+        
         private UserDto CreateUserObject(AppUser user)
         {
             return new UserDto
--- API/Startup.cs
+++ API/Startup.cs
@@ -61,12 +61,17 @@ namespace API
             app.UseXfo(opt => opt.Deny());
             app.UseCsp(opt => opt
                 .BlockAllMixedContent()
-                .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
+                .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com","sha256-oFySg82XYSNiSd+Q3yfYPD/rxY6RMDMJ0KxzGG74iGM="))
                 .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
                 .FormActions(s => s.Self())
                 .FrameAncestors(s => s.Self())
-                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
-                .ScriptSources(s => s.Self().CustomSources("sha256-HIgflxNtM43xg36bBIUoPTUuo+CXZ319LsTVRtsZ/VU="))
+                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com","https://www.facebook.com",  "https://platform-lookaside.fbsbx.com/"))
+                .ScriptSources(s => s.Self()
+                    .CustomSources("sha256-HIgflxNtM43xg36bBIUoPTUuo+CXZ319LsTVRtsZ/VU=", 
+                        "https://connect.facebook.net",
+                        "sha256-HIgflxNtM43xg36bBIUoPTUuo+CXZ319LsTVRtsZ/VU=",
+                        "sha256-7Lb47EwmUDjzk9UrmTecz2dfBOOC0+6ijyE94GjUlLE="
+                        ))
             );
             
             if (env.IsDevelopment())
